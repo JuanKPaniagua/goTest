27be74dcacb992d786ea7817b3cde8706a033a8b
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-08-09 16:56:25 -0600

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -70,17 +70,14 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	dataJson,err = json.Marshal(r.Form)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(dataJson)*/
-	var u User
-        if r.Body == nil {
-            http.Error(w, "Please send a request body", 400)
-            return
-        }
-        err := json.NewDecoder(r.Body).Decode(&u)
-        if err != nil {
-            http.Error(w, err.Error(), 400)
-            return
-        }
-        fmt.Println(u)
+	err = req.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+	v := req.Form
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(v)
+    w.WriteHeader(200)
     return
 }
 
