c69aca4a572731e41148ad6951637f439861f812
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-08-08 20:02:14 -0600

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -62,13 +62,13 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
 	id := path.Base(r.URL.Path)
 	checkError("Parse error", err)
-	i := dlete(id)
+	i := delete(id)
 	if i == -1 {
 		//w.Header().Set("Content-Type", "application/json")
 		//w.Write("Id typed cannot be founded")	
 		return
 	}
-	dataJson,err = json.Marshal(books)
+	dataJson,err := json.Marshal(books)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(dataJson)
     w.WriteHeader(200)
