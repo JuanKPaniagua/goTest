3ad60dad8468bddeea3c70e5e75208d404385aa0
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-08-09 17:27:32 -0600

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -56,10 +56,10 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	decoder := json.NewDecoder(r.Body)
 	var data Book
-	err = decoder.Decode(&data)
+	/*err = decoder.Decode(&data)
 	if err != nil {
 		return
-	}
+	}*/
 	id := path.Base(r.URL.Path)
 	i := find(id)
 	if i == -1 {
