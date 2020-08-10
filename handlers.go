d1e81ac4bbe65af7145fc4d06960b4e57188893e
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-08-09 20:59:53 -0600

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -15,16 +15,30 @@ func find(x string) int {
     return -1
 }
 
+func delete(x string) int {
+    for i, book := range books {
+        if x == book.Id {
+			copy(books[i:], books[i+1:]) // Shift a[i+1:] left one index.
+			books = books[:len(books)-1]     // Truncate slice.
+            return i
+        }
+    }
+    return -1
+}
+
 func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
     id := path.Base(r.URL.Path)
-    checkError("Parse error", err)
-    i := find(id)
-    if i == -1 {
-        return
-    }
-    dataJson, err := json.Marshal(books[i])
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(dataJson)
+	checkError("Parse error", err)
+	i := find(id)
+	dataJson,err := json.Marshal(books)
+	if i == -1 {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(dataJson)	
+		return
+	}
+	dataJson,err = json.Marshal(books[i])
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dataJson)	
     return
 }
 
@@ -45,6 +59,17 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 }
 
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+	id := path.Base(r.URL.Path)
+	checkError("Parse error", err)
+	i := delete(id)
+	if i == -1 {
+		//w.Header().Set("Content-Type", "application/json")
+		//w.Write("Id typed cannot be founded")	
+		return
+	}
+	dataJson,err := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dataJson)
     w.WriteHeader(200)
     return
 }
