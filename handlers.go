112aaac2cedab104cacb7099396b2df2132293c8
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-08-11 22:58:09 -0600

    Add files via upload

--- handlers.go
+++ handlers.go
@@ -54,10 +54,43 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
-    w.WriteHeader(200)
-    return
+	id := path.Base(r.URL.Path)
+	i := find(id)
+	if i == -1 {
+		return
+	}	
+	//r.ParseForm()
+	len := r.ContentLength
+    body := make([]byte, len)
+    r.Body.Read(body)
+    book := Book{}
+    json.Unmarshal(body, &book)
+	if book.Title != ""{
+		books[i].Title=book.Title
+	}
+	if book.Edition != ""{
+		books[i].Edition=book.Edition
+	}
+	if book.Copyright != ""{
+		books[i].Copyright=book.Copyright
+	}
+	if book.Language != ""{
+		books[i].Language=book.Language
+	}
+	if book.Pages != ""{
+		books[i].Pages=book.Pages
+	}
+	if book.Author != ""{
+		books[i].Author=book.Author
+	}
+	if book.Publisher != ""{
+		books[i].Publisher=book.Publisher
+	}
+	dataJson,err := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(dataJson)
+	return
 }
-
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
 	id := path.Base(r.URL.Path)
 	checkError("Parse error", err)
