5a6a6063b8438a697a3f0d8c448364ef41693ed6
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-09-22 17:05:50 -0600

    Test1

--- service.go
+++ service.go
@@ -16,6 +16,22 @@ type Book struct {
     Publisher string `json:"publisher,omitempty"`
 }
 
+type Publisher struct {
+    PublisherId    string `json:"publisherId,omitempty"`
+    Name     string `json:"name,omitempty"`
+    Country   string `json:"country,omitempty"`
+    Founded string `json:"founded,omitempty"`
+    Genere  string `json:"genere,omitempty"`
+}
+
+type Author struct {
+    AuthorId    string `json:"authorId,omitempty"`
+    Name     string `json:"name,omitempty"`
+    Nationality   string `json:"nacionality,omitempty"`
+    Birth string `json:"birth,omitempty"`
+    Genere  string `json:"genere,omitempty"`
+}
+
 type bookservice struct {
     logger log.Logger
 }
@@ -31,13 +47,29 @@ type BookService interface {
 var books = []Book{
     Book{BookId: "Book1", Title: "Operating System Concepts", Edition: "9th",
         Copyright: "2012", Language: "ENGLISH", Pages: "976",
-        Author: "Abraham Silberschatz", Publisher: "John Wiley & Sons"},
-    Book{BookId: "Book3", Title: "Computer Networks", Edition: "5th",
+        Author: "Author2", Publisher: "Publisher2"},
+    Book{BookId: "Book2", Title: "Computer Networks", Edition: "5th",
         Copyright: "2010", Language: "ENGLISH", Pages: "960",
-        Author: "Andrew S. Tanenbaum", Publisher: "Andrew S. Tanenbaum"},
+        Author: "Author1", Publisher: "Publisher1"},
 }
 
-func find(x string) int {
+var authors = []Author{
+    Author{AuthorId: "Author1", Name: "Andrew S. Tanenbaum", Nationality: "Canada",
+        Birth: "1977", Genere: "Programming"},
+    Author{AuthorId: "Author2", Name: "Abraham Silberschatz", Nationality: "US",
+        Birth: "1949", Genere: "Programming"},
+}
+
+var publishers = []Publisher{
+    Publisher{PublisherId: "Publisher1", Name: "John Wiley & Sons", Country: "UK",
+        Founded: "1994", Genere: "Programming"},
+    Publisher{PublisherId: "Publisher2", Name: "Mc Graw Hill", Country: "US",
+        Founded: "1919", Genere: "Teaching"},
+}
+
+//Finders
+
+func findBooks(x string) int {
     for i, book := range books {
         if x == book.BookId {
             return i
@@ -45,6 +77,47 @@ func find(x string) int {
     }
     return -1
 }
+
+func findPublishers(x string) int {
+    for i, publisher := range publishers {
+        if x == publisher.PublisherId {
+            return i
+        }
+    }
+    return -1
+}
+
+func findAuthors(x string) int {
+    for i, author := range authors {
+        if x == author.AuthorId {
+            return i
+        }
+    }
+    return -1
+}
+
+func findBooksbyAuthors(x string) []int {
+	y:=[]int{}
+    for i, _ := range books {
+		for _,z := range books[i].Author{
+			if z == x {
+				y = append(y, i)
+			}
+		} 
+    }
+    return y
+}
+
+func findBooksbyPublishers (x string) []int {
+	y:=[]int{}
+    for i, book := range books {
+		if x == book.Publisher {
+			y = append(y, i)
+		}
+    }
+    return y
+}
+
 
 func NewService(logger log.Logger) BookService {
     return &bookservice{
@@ -62,7 +135,7 @@ func (s bookservice) GetBookById(ctx context.Context, id string) (interface{}, e
     var err error
     var book interface{}
     var empty interface{}
-    i := find(id)
+    i := findBooks(id)
     if i == -1 {
         return empty, err
     }
@@ -72,7 +145,7 @@ func (s bookservice) GetBookById(ctx context.Context, id string) (interface{}, e
 func (s bookservice) DeleteBook(ctx context.Context, id string) (string, error) {
     var err error
     msg := ""
-    i := find(id)
+    i := findBooks(id)
     if i == -1 {
         return "", err
     }
@@ -85,7 +158,7 @@ func (s bookservice) UpdateBook(ctx context.Context, book Book) (string, error) 
     var empty = ""
     var err error
     var msg = "success"
-    i := find(book.BookId)
+    i := findBooks(book.BookId)
     if i == -1 {
         return empty, err
     }
